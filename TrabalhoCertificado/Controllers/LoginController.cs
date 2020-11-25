2173099e45fb980a44e7aeb1de9ab93f31c3dfc1
lockade <jeffersondias-@hotmail.com>
2020-11-25 20:07:48 -0300

    First

---
+++ TrabalhoCertificado/Controllers/LoginController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using TrabalhoCertificado.Models;
+
+namespace TrabalhoCertificado.Controllers
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                if (login.Email == "admin@admin.com" && login.Senha == "admin")
+                {
+                    List<Claim> claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Sid, "1"),
+                        new Claim(ClaimTypes.Name, "Jefferson"),
+                        new Claim(ClaimTypes.Role, "A"),
+                    };
+
+                    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                    AuthenticationProperties authProperties = new AuthenticationProperties
+                    {
+                        AllowRefresh = true,
+                        ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                        IsPersistent = true,
+                    };
+
+                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ViewBag.Erro = "Usuário e/ou senha inválidos";
+            return View();
+
+        }
+    }
+}
---
+++ TrabalhoCertificado/Models/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoCertificado.Models
+{
+    public class Login
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [EmailAddress(ErrorMessage = "Informe um e-mail válido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Utilize ao menos 4 caracteres")]
+        [MaxLength(32, ErrorMessage = "Utilize ao máximo 32 caracteres")]
+        public string Senha { get; set; }
+    }
+}
