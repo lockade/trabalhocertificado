26979c910e97311f61fe8dfb064d28748834cd6d
guzztavo2 <62066122+guzztavo2@users.noreply.github.com>
2020-12-20 05:29:41 -0300

    Inserção da "ViewModel" de atividade e tipo de atividade, para a inserção de duas models dentro de uma mesma view. Entretanto, a parte funcional do site não está funcionando. Apenas a view e o banco de dados foram criados. Falta o back-end.

--- TrabalhoCertificado/Controllers/AtividadeController.cs
+++ TrabalhoCertificado/Controllers/AtividadeController.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TrabalhoCertificado.Data;
 
 namespace TrabalhoCertificado.Controllers
 {
     public class AtividadeController : Controller
     {
-        public IActionResult Index()
+        private readonly DataContext context;
+
+        public AtividadeController(DataContext _context)
+        {
+            context = _context;
+        }
+
+        public ActionResult Index()
+        {
+            var livros = context.TBAtividades.ToList();
+            return View(livros);
+        }
+        public ActionResult Create()
         {
             return View();
         }
---
+++ TrabalhoCertificado/Models/AtividadeLink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabalhoCertificado.Models
+{
+    public class AtividadeLink
+    {
+        public List<Atividade> atividades { get; set; }
+        public Atividade atividade { get; set; }
+        public TipoAtividade tipoAtividade {get;set;}
+        public List<TipoAtividade> tipoAtividades { get; set; }
+    }
+}
